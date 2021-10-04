8bed5815f65961aee0a1535db739eabf54f18e13
Christian Hoffmann <cho@uni-tel.dk>
2021-10-04 10:16:10 +0200

    view tilpasning load af omsæt

--- BASELINE/Views/FACT_OMSÆTNING_V.sql
+++ BASELINE/Views/FACT_OMSÆTNING_V.sql
@@ -1,4 +1,4 @@
-CREATE VIEW [BASELINE].[FACT_OMSÆTNING_V]
+ALTER VIEW [BASELINE].[FACT_OMSÆTNING_V]
 AS
 (
 SELECT 
@@ -6,14 +6,20 @@ N.[Line No_] AS LINJE_NR,
 CAST (N.[Posting Date] AS DATE) AS BOGFØRINGSDATO, 
 DATEADD (D, 14, CAST (N.[Posting Date] AS DATE)) AS BETALINGSFRIST,
 N.[Document No_] AS FAKTURA_NR,
-N.DESCRIPTION AS BESKRIVELSE,
-N.[Sell-to Customer No_] as KUNDE_ID,
+N.DESCRIPTION AS BESKRIVELE,
+-- [Sell-to Customer No_] er nvchar i kilden
+case
+		when ISNUMERIC([Sell-to Customer No_])=1 then cast ([Sell-to Customer No_] as int) 
+		when lower([Sell-to Customer No_]) = 'FIRSTCOM' then 18
+		else null 
+		end as KUNDE_ID,
 N.[NO_ ] AS RESSOURCEGROUP_ID,
 CAST (N.Quantity AS DECIMAL (38,20)) AS ANTAL_ENHEDER, 
 CAST (N.[Unit Price] AS DECIMAL (38,20)) AS ENHEDSPRIS,
 CAST (N.Quantity AS DECIMAL (38,20)) * CAST (N.[Unit Price] AS DECIMAL (38,20)) AS BELØB_EKSKL_MOMS,
-n.[Line Discount _] as LINJE_RABAT,
-n.[Line Discount Amount] as LINJE_RABAT_BELØB
+N.[Line Discount _] as LINJE_RABAT,
+N.[Line Discount Amount] as LINJE_RABAT_BELØB,
+N.[load_dato]
 FROM navision.[Uni-tel_AS_Sales_Invoice_Line] N
 )
 GO
