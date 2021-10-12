0fa56bc35ed84cf3338ab8d0fde7f51cba29530d
Christian Hoffmann <cho@uni-tel.dk>
2021-10-12 11:01:25 +0200

    DELTA

--- BASELINE/Views/FACT_OMSÆTNING_V.sql
+++ BASELINE/Views/FACT_OMSÆTNING_V.sql
@@ -1,8 +1,9 @@
-ALTER VIEW [BASELINE].[FACT_OMSÆTNING_V_NEW]
+ALTER VIEW [BASELINE].[FACT_OMSÆTNING_V]
 AS
 (
 SELECT 
 N.[Line No_] AS LINJE_NR,
+--Posting Date is used here as apposed to shipment date (used by isaksen)
 N.[Posting Date] AS BOGFØRINGSDATO, 
 N.[Document No_] AS FAKTURA_NR,
 N.[Description] AS BESKRIVELSE,
@@ -15,12 +16,11 @@ CASE
 N.[No_] AS RESSOURCEGROUP_ID,
 N.Quantity AS ANTAL_ENHEDER, 
 N.[Unit Price] AS ENHEDSPRIS,
-N.Quantity * N.[Unit Price] AS BELØB_EKSKL_MOMS,
+N.[Amount] AS BELØB_EKSKL_MOMS,
 N.[Line Discount _] AS LINJE_RABAT,
 N.[Line Discount Amount] AS LINJE_RABAT_BELØB,
 N.[load_dato] AS LOAD_DATO
 FROM navision.[Uni-tel_AS_Sales_Invoice_Line] N
 )
 GO
-
 
