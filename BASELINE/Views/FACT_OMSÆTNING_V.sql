5428ccad6de37da664a0bb747050f592b2e198b9
Christian Hoffmann <cho@uni-tel.dk>
2021-10-12 10:26:09 +0200

    NY VER

--- BASELINE/Views/FACT_OMSÆTNING_V.sql
+++ BASELINE/Views/FACT_OMSÆTNING_V.sql
@@ -1,25 +1,24 @@
-ALTER VIEW [BASELINE].[FACT_OMSÆTNING_V]
+ALTER VIEW [BASELINE].[FACT_OMSÆTNING_V_NEW]
 AS
 (
 SELECT 
 N.[Line No_] AS LINJE_NR,
-CAST (N.[Posting Date] AS DATE) AS BOGFØRINGSDATO, 
-DATEADD (D, 14, CAST (N.[Posting Date] AS DATE)) AS BETALINGSFRIST,
+N.[Posting Date] AS BOGFØRINGSDATO, 
 N.[Document No_] AS FAKTURA_NR,
-N.DESCRIPTION AS BESKRIVELSE,
+N.[Description] AS BESKRIVELSE,
 -- [Sell-to Customer No_] er nvchar i kilden
-case
-		when ISNUMERIC([Sell-to Customer No_])=1 then cast ([Sell-to Customer No_] as int) 
-		when lower([Sell-to Customer No_]) = 'FIRSTCOM' then 18
-		else null 
-		end as KUNDE_ID,
-N.[NO_ ] AS RESSOURCEGROUP_ID,
-CAST (N.Quantity AS DECIMAL (38,20)) AS ANTAL_ENHEDER, 
-CAST (N.[Unit Price] AS DECIMAL (38,20)) AS ENHEDSPRIS,
-CAST (N.Quantity AS DECIMAL (38,20)) * CAST (N.[Unit Price] AS DECIMAL (38,20)) AS BELØB_EKSKL_MOMS,
-N.[Line Discount _] as LINJE_RABAT,
-N.[Line Discount Amount] as LINJE_RABAT_BELØB,
-N.[load_dato]
+CASE
+		WHEN ISNUMERIC([Sell-to Customer No_])=1 THEN CAST ([Sell-to Customer No_] as int) 
+		WHEN LOWER([Sell-to Customer No_]) = 'FIRSTCOM' THEN 18
+		ELSE null 
+		END AS KUNDE_ID,
+N.[No_] AS RESSOURCEGROUP_ID,
+N.Quantity AS ANTAL_ENHEDER, 
+N.[Unit Price] AS ENHEDSPRIS,
+N.Quantity * N.[Unit Price] AS BELØB_EKSKL_MOMS,
+N.[Line Discount _] AS LINJE_RABAT,
+N.[Line Discount Amount] AS LINJE_RABAT_BELØB,
+N.[load_dato] AS LOAD_DATO
 FROM navision.[Uni-tel_AS_Sales_Invoice_Line] N
 )
 GO
