e961803bcb0d35d6b277a38f8863c66cfbfd9f04
Christian Hoffmann <cho@uni-tel.dk>
2021-10-29 11:20:24 +0200

    OMSÆTNING - TILKOBLING AF DATE_ID

--- BASELINE/Views/FACT_OMSÆTNING_V.sql
+++ BASELINE/Views/FACT_OMSÆTNING_V.sql
@@ -2,11 +2,7 @@ ALTER VIEW [BASELINE].[FACT_OMSÆTNING_V]
 AS
 (
 SELECT 
-N.[Line No_] AS LINJE_NR,
---Posting Date is used here as apposed to shipment date (used by isaksen)
-N.[Posting Date] AS BOGFØRINGSDATO, 
-N.[Document No_] AS FAKTURA_NR,
-N.[Description] AS BESKRIVELSE,
+--DIM REFERENCES 
 -- [Sell-to Customer No_] er nvchar i kilden
 CASE
 		WHEN ISNUMERIC([Sell-to Customer No_])=1 THEN CAST ([Sell-to Customer No_] as int) 
@@ -14,13 +10,23 @@ CASE
 		ELSE null 
 		END AS KUNDE_ID,
 N.[No_] AS RESSOURCEGROUP_ID,
-N.Quantity AS ANTAL_ENHEDER, 
-N.[Unit Price] AS ENHEDSPRIS,
-N.[Amount] AS BELØB_EKSKL_MOMS,
+EOMONTH(N.[Posting Date]) AS DATE_ID,
+
+--ATTRIBUTS
+N.[Line No_] AS LINJE_NR,
+--Posting Date is used here as apposed to shipment date (used by isaksen)
+N.[Posting Date] AS BOGFØRINGSDATO, 
+N.[Document No_] AS FAKTURA_NR,
+N.[Description] AS BESKRIVELSE,
 N.[Line Discount _] AS LINJE_RABAT,
 N.[Line Discount Amount] AS LINJE_RABAT_BELØB,
-N.[load_dato] AS LOAD_DATO
+N.[load_dato] AS LOAD_DATO,
+
+--MEASURES 
+N.Quantity AS ANTAL_ENHEDER, 
+N.[Unit Price] AS ENHEDSPRIS,
+N.[Amount] AS BELØB_EKSKL_MOMS
 FROM navision.[Uni-tel_AS_Sales_Invoice_Line] N
 )
-GO
+
 
