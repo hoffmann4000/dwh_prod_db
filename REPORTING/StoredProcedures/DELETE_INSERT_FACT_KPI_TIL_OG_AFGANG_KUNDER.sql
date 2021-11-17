016259a7fb73c2739184f90ddb3401314472d143
Christian Hoffmann <cho@uni-tel.dk>
2021-11-17 13:16:08 +0100

    change

--- REPORTING/StoredProcedures/DELETE_INSERT_FACT_KPI_TIL_OG_AFGANG_KUNDER.sql
+++ REPORTING/StoredProcedures/DELETE_INSERT_FACT_KPI_TIL_OG_AFGANG_KUNDER.sql
@@ -1,42 +1,37 @@
-CREATE PROCEDURE [REPORTING].[DELETE_INSERT_FACT_KPI_TIL_OG_AFGANG_KUNDER]
+CREATE  PROCEDURE [REPORTING].[DELETE_INSERT_FACT_KPI_TIL_OG_AFGANG_KUNDER]
 AS 
 BEGIN
 IF OBJECT_ID('REPORTING.FACT_KPI', 'U') IS NOT NULL 
 
-PRINT 'Der slettes rækker i REPORTING.FACT_KPI for to KPIer - afgang og tilgang af kunder';
+PRINT 'Rows are deleted in REPORTING.FACT_KPI for two KPIs - Tilgang og afgang af kunder - number of added and departing customers';
 
-DELETE FROM REPORTING.FACT_KPI WHERE KPI_ID=(SELECT KPI_ID FROM REPORTING.DIM_KPI WHERE KPI_NAVN = 'Tilgang af kunder');
-DELETE FROM REPORTING.FACT_KPI WHERE KPI_ID=(SELECT KPI_ID FROM REPORTING.DIM_KPI WHERE KPI_NAVN = 'Afgang af kunder');
-
+	DELETE FROM REPORTING.FACT_KPI WHERE KPI_ID in (73, 72);
 		
-PRINT 'KPIet "Tilgang af kunder" indsættes i FACT_KPI';
+PRINT 'the KPI for number of added customers "Tilgang af kunder" is inserted into REPORTING.FACT_KPI';
 	
 	INSERT INTO REPORTING.FACT_KPI (REALISERET, FORHANDLER_ID, DATE_ID, KPI_ID)
-	
 	SELECT 
 	TILGANG_KUNDER AS REALISERET,
 	A.FORHANDLER_ID,
 	T.LASTDAYOFMONTH as DATE_ID, 
-	(SELECT KPI_ID FROM REPORTING.DIM_KPI WHERE KPI_NAVN='Tilgang af kunder') AS KPI_ID
-		
+	72 AS KPI_ID
 	FROM BASELINE.LASTDAYOFMONTH_ONE_YEAR_BACK T  
-		INNER JOIN   
-			(
-			SELECT COUNT(DISTINCT K.KUNDE_ID) AS TILGANG_KUNDER, EOMONTH(K.KUNDESTARTDATO) AS LAST_DAY_OF_MONTH_DT,  K.FORHANDLER_ID
-			FROM REPORTING.DIM_KUNDE K
-			GROUP BY EOMONTH(KUNDESTARTDATO), K.FORHANDLER_ID
-			) A 
+	INNER JOIN   
+		(
+		SELECT COUNT(DISTINCT K.KUNDE_ID) AS TILGANG_KUNDER, EOMONTH(K.KUNDESTARTDATO) AS LAST_DAY_OF_MONTH_DT,  K.FORHANDLER_ID
+		FROM REPORTING.DIM_KUNDE K
+		GROUP BY EOMONTH(KUNDESTARTDATO), K.FORHANDLER_ID
+		) A 
 	ON T.LASTDAYOFMONTH=A.LAST_DAY_OF_MONTH_DT;
 
-PRINT 'KPI "Afgang af kunder indsættes i FACT_KPI'
+PRINT 'the KPI for number of departing customers "Afgang af kunder" is inserted into REPORTING.FACT_KPI';
 	INSERT INTO REPORTING.FACT_KPI (REALISERET, FORHANDLER_ID, DATE_ID, KPI_ID)
 	
 		SELECT 
 		AFGANG_KUNDER AS REALISERET,
 		A.FORHANDLER_ID,
 		T.LASTDAYOFMONTH as DATE_ID, 
-		(SELECT KPI_ID FROM REPORTING.DIM_KPI WHERE KPI_NAVN='Afgang af kunder') AS KPI_ID
-		
+		73 AS KPI_ID
 		FROM BASELINE.LASTDAYOFMONTH_ONE_YEAR_BACK T  
 			INNER JOIN   
 				(
@@ -47,11 +42,9 @@ PRINT 'KPI "Afgang af kunder indsættes i FACT_KPI'
 				) A 
 		ON T.LASTDAYOFMONTH=A.LAST_DAY_OF_MONTH_DT;
 /*
-Chr 23.6.2021
-Tilgang af kunder er en simpel optælling af distinkte kunder pr. forhandler pr. måned baseret på feltet ADMIN.CUST.CREATED udstilles i REPORTING.DIM_KUNDE
-Afgang af kunder beregnes ved at se ned i REPORTING.DIM_KUNDE, hvor grunddata udgøres af tabellen ADMIN.CHURN, som supplerer admin.cust med en kunde-slutdato efter 1.1.2016
-*/
+Christian 2021-01-17
+Incoming customers is a simple count of distinct customers per. dealer per. month based on the field ADMIN.CUST.CREATED is displayed in REPORTING.DIM_KUNDE
+Departure of customers is calculated by looking down in REPORTING.DIM_KUNDE, where basic data is made up of the table ADMIN.CHURN, which supplements admin.cust with a customer end date after 1.1.2016 */
 END;
 
-GO
 
