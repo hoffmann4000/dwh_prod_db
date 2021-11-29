f125dc60776b9925f8f6b63505d378338ad5250b
Christian Hoffmann <cho@uni-tel.dk>
2021-11-29 15:12:00 +0100

    -

--- admin/delete_columns.sql
+++ admin/delete_columns.sql
@@ -1,6 +1,17 @@
-CREATE PROCEDURE [dbo].[delete_columns]
-  @param1 int = 0,
-  @param2 int
-AS
-  SELECT @param1, @param2
-RETURN 0
+alter procedure admin.delete_columns
+as
+--Christian 29.11.2021
+--Azure Data Factory loads admin source table in a loop - table by table
+--there is no option for deselecting specific columns 
+--the better solution would be to change the load into the DWH server
+print ('the column admin.cust.adminpass is deleted');
+update admin.cust
+set adminpass = null;
+
+print ('the column admin.cust.bookiepass is deleted');
+update admin.cust
+set bookiepass = null;
+
+print ('the column admin.forhandlere.[password] is deleted');
+update admin.forhandlere
+set [password] = null;
